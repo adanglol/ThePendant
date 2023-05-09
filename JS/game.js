3680adb0bd924fe4b224fad3f9d3149d49187252
Aaron Dangc <adraging@gmail.com>
2023-05-09 01:19:08 -0700

    added fullscreen and Game Config

---
+++ JS/game.js
@@ -0,0 +1,76 @@
+// Author : Aaron Dangc
+
+// Ensure Our Game runs
+console.log("Game is running");
+
+function addFullscreenButton(scene) {
+    // Add fullscreen button
+    const fullScreenButton = scene.add.image(config.scale.width - 16, 16, 'fullscreen', 0)
+      .setOrigin(.9, 0.1)
+      .setInteractive()
+      .setScale(0.1);
+      
+    fullScreenButton.on('pointerup', function () {
+      if (scene.scale.isFullscreen) {
+        fullScreenButton.setFrame(0);
+        scene.scale.stopFullscreen();
+      } else {
+        fullScreenButton.setFrame(1);
+        scene.scale.startFullscreen();
+      }
+    });
+  }
+  
+
+
+
+
+
+// function addFullscreenButton(scene) {
+//     const fullScreenButton = scene.add.image(800-16, 16, 'fullscreen', 0).setOrigin(1, 0).setInteractive();
+//     fullScreenButton.on('pointerup', function () {
+//         if (this.scale.isFullscreen) {
+//             fullScreenButton.setFrame(0);
+//             this.scale.stopFullscreen();
+//         } else {
+//             fullScreenButton.setFrame(1);
+//             this.scale.startFullscreen();
+//         }
+//     }, scene);
+// }
+
+    
+
+
+class Menu extends Phaser.Scene {
+    constructor() {
+      super('Menu');
+    }
+  
+    preload() {
+      this.load.image('fullscreen', 'lib/assets/fullscreen.png');
+    }
+  
+    create() {
+      // Add fullscreen button
+        addFullscreenButton(this);
+  
+      
+    }
+  }
+  
+  const config = {
+    type: Phaser.AUTO,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      parent: 'game-container',
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 800,
+      height: 600
+    },
+    scene: [ Menu ],
+    backgroundColor: '#8fffff'
+  };
+  
+  const game = new Phaser.Game(config);
+  
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ThePendant
 A platformer based game created using Phaser.
+
+
+[Markdown link syntax](https://www.markdownguide.org/basic-syntax/#links)
+
+ATTRIBUTIONS (credit where credit is due):
+
+Full Screen Icon : 
+ [Markdown link syntax](https://www.flaticon.com/free-icons/fullscreen) - Fullscreen icons created by afif fudin
