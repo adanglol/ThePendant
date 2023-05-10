d56e4055b68882c6868e54054a6db91e39c35807
Aaron Dangc <adraging@gmail.com>
2023-05-10 02:17:58 -0700

    moving character and platform with squares need implement platforms and level

--- JS/game.js
+++ JS/game.js
@@ -96,10 +96,83 @@ class Menu extends Phaser.Scene {
     create() {
       // Add fullscreen button
       addFullscreenButton(this);
-      // Add pause button
+
+      // change background color of 
+      this.cameras.main.setBackgroundColor('#ffff00');
+
+      // Create a static group
+      this.platforms = this.physics.add.staticGroup();
+
+      // Create the ground as a rectangle and add it to the group
+      let ground = this.add.rectangle(400, 580, 800, 50, 0xf000f0);
+      this.physics.add.existing(ground, true);
+      ground.body.immovable = true;
+      this.platforms.add(ground);
+
      
+      
+      // Create other rectangles and add them to the group - OUR PLATFORMS
+      let platform1 = this.add.graphics();
+      platform1.fillStyle(0x000000);
+      platform1.fillRect(500, 400, 200, 25);
+      this.physics.add.existing(platform1, true);
+      platform1.body.immovable = true;
+      this.platforms.add(platform1);
+
+      let platform2 = this.add.graphics();
+      platform2.fillStyle(0x000000);
+      platform2.fillRect(100, 250, 100, 25);
+      this.physics.add.existing(platform2, true);
+      platform2.body.immovable = true;
+      this.platforms.add(platform2);
+
+      let platform3 = this.add.graphics();
+      platform3.fillStyle(0x000000);
+      platform3.fillRect(650, 200, 150, 25);
+      this.physics.add.existing(platform3, true);
+      platform3.body.immovable = true;
+      this.platforms.add(platform3);
+
+      // player
+      let playerSprite = this.add.graphics();
+      playerSprite.fillStyle(0x0000ff);
+      playerSprite.fillRect(0, 0, 50, 50);
+      playerSprite.generateTexture('player', 50, 50);
+      playerSprite.destroy();
+      this.player = this.physics.add.sprite(400,405,'player');
+      this.player.setScale(5.3);
+      this.physics.add.existing(this.player, true);
+      this.player.body.setCollideWorldBounds(true);
+
+      // so player does not fall
+      this.physics.add.collider(this.player, this.platforms);
+
+      // add keyboard controls
+      this.cursors = this.input.keyboard.createCursorKeys();
+
+
+
+      
+
+
+            
     }
     update() {
+      // player movement left and right
+      if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-260);
+      } else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(160);
+      } else {
+        this.player.setVelocityX(0);
+      }
+
+      // player jump
+      if (this.cursors.up.isDown && this.player.body.touching.down) {
+        this.player.setVelocityY(-330);
+      }
+
+
       
 
     }
@@ -127,11 +200,18 @@ class Menu extends Phaser.Scene {
       width: 800,
       height: 600,
     },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 500 },
+        debug: true
+      },
+    },
     style : {
       border : '10px solid black'
     },
-    scene: [ Menu, Game],
-    backgroundColor: '#8fffff'
+    scene: [ Game, Menu ],
+    backgroundColor: '#8fffff',
   };
   
   const game = new Phaser.Game(config);
